d5057165f1d6153e0615d395b5b27043945b1766
Shelkopryad <victoor.yakimov@gmail.com>
2017-05-22 09:49:26 +0400

    delete calculator class

--- src/main/java/ru/sbt/Calculator.java
+++ src/main/java/ru/sbt/Calculator.java
@@ -1,94 +1,37 @@
 package ru.sbt;
 
-import java.util.Scanner;
 
-/**
- * Created by victo on 21.05.2017.
- */
 public class Calculator {
 
-    private static Scanner read;
 
-    public static void main (String[] args){
-
-
-        while (true){
-
-            Maths maths = new Maths();
-
-            double first;
-            double second;
-            Character operator;
-            double answer = 0;
-
-            System.out.print(">> ");
-
-            first = readDouble();
-            operator = readOperation();
-            second = readDouble();
-
-            switch (operator) {
-                case '+': answer = maths.add(first, second);
-                    break;
-                case '-': answer = maths.subtract(first, second);
-                    break;
-                case '*': answer = maths.multiply(first, second);
-                    break;
-                case '/': answer = maths.divide(first, second);
-                    break;
-                case '^': answer = maths.power(first, second);
-                    break;
-            }
-
-            System.out.println("= " + answer);
-
-
-        }
-
+    public double add(double a, double b) {
+        double answer = a+b;
+        return answer;
     }
 
-    private static Character readOperation() {
-
-        read = new Scanner(System.in);
-
-        while (true) {
-
-            String c = read.next();
-
-            char operation = c.trim().charAt(0);
-
-            switch (operation) {
-                case '-':
-                case '+':
-                case '*':
-                case '/':
-                case '^':
-                    return operation;
-
-                default:
-                    System.out.println("Неизвестная операция. Введите еще раз");
-            }
-        }
+    public double subtract(double a, double b) {
+        double answer = a-b;
+        return answer;
     }
 
-    private static Double readDouble(){
-
-        read = new Scanner(System.in);
-
-        while (true){
-
-            String c = read.next();
-
-            try{
-
-                return Double.parseDouble(c.trim());
-
-            }catch (Exception e ){
-
-                System.out.println("Введенное значение не является чилом. Введите другое значение ! ");
-            }
-        }
+    public double multiply(double a, double b) {
+        double answer = a*b;
+        return answer;
     }
 
+    public double divide(double a, double b) {
+        double answer = a/b;
+        return answer;
+    }
+
+    public double power(double a, double b){
+        double answer =a;
+
+        for (int x=2; x<=b; x++){
+            answer *= a;
+        }
+
+        return answer;
+    }
 
 }
